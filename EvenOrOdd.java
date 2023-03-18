9a03e2f2084bb161ee0163835639dfad30e2d611
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2023-03-18 19:21:07 +0530

    JavaPrograms

---
+++ EvenOrOdd.java
@@ -0,0 +1,33 @@
+public class EvenOrOdd {
+
+    public static boolean isEvenOrOdd(int num){
+        if (num<=1) {
+            return false;
+        }
+
+        for (int index = 2; index < num; index++) {
+            if (num%2!=0) {
+                return false;
+             }
+            
+        }
+        return true;
+    }
+
+    public static void evenNumbers(int num){
+        for (int index = 2; index <= num; index++) {
+            if (isEvenOrOdd(index)) {
+                System.out.println(index);
+            } 
+            
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        evenNumbers(100);
+    }
+}
--- Prac.java
+++ Prac.java
@@ -2,21 +2,24 @@
 public class Prac {
 
     public static void solution(){
-        int[] arr={1,2,4,5,6};
-        int sum=0;
-        int sum2=0;
-        for (int index = 0; index < arr.length; index++) {
-            sum=sum+arr[index];
+        int num=100;
+
+        for (int index = 1; index <=num; index++) {
+            if (index%3==0 && index%5==0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (index%3==0) {
+                System.out.println("Fizz");
+            }
+            else if (index%5==0) {
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(index);
+            }
             
         }
-        System.out.println(sum);
-
-
-        for (int index = 0; index <=6; index++) {
-            sum2+=index;
-        }
-        System.out.println(sum2);
-        System.out.println(sum2-sum);
+     
     }
 
 
